7672b06dcba57b3c4190dd2be405d27b8d575119
clanlop <clanlop@hotmail.com>
2022-06-26 23:45:48 -0300

    mi tercer commit

---
+++ ProyectoIntegrador_claudiaLopez/BackEnd/src/main/java/com/portfolio/clanlop/ClanlopApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.clanlop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClanlopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClanlopApplication.class, args);
+	}
+
+}
